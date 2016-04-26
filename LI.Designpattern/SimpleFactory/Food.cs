67ea4cedb547617635be8b109b67fa333889c6e9
孤鸿 夜 <litiansum@foxmail.com>
2016-04-26 13:30:46 +0800

    factory

---
+++ LI.Designpattern/SimpleFactory/Food.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LI.Designpattern
+{ 
+    /// <summary>
+    /// 菜抽象类
+    /// </summary>
+    abstract class Food
+    {
+        /// <summary>
+        /// 输出食物
+        /// </summary>
+        public abstract void Print();
+    }
+}

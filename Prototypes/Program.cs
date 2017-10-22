b3c4891e336740d81f34939f9d78898e8841f71c
孤鸿 夜 <litiansum@foxmail.com>
2017-10-22 19:57:45 +0800

    添加原型模式

---
+++ Prototypes/Program.cs
@@ -0,0 +1,33 @@
+﻿/***
+ * 原型模式：用原型实例指定创建对象的种类，并且通过拷贝这些原型创建新的对象。
+ *          原型模式其实就是从一个对象再创建另一个可定制的对象，且不需要知道任何创建过程。
+ * 
+ * 在.NET中使用System命名空间下的ICloneable接口即可实现原型模式。
+ * 
+ * 浅复制vs深复制
+ * MemberwiseClone()方法是浅复制,原理：如果字段是值类型则，则对该字段执行逐位复制，如果是引用类型，则复制引用而不复制引用的对象。
+ * 深复制：把引用对象的变量指向复制过来的新对象，而不是原有的被引用的对象。
+ */
+
+
+using System;
+
+namespace Prototypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Resume a = new Resume("小李");
+            a.SetPersonalInfo("男", 26);
+            a.SetWorkExperience("2015-2017", "IWIT");
+
+            Resume b = (Resume)a.Clone();
+            b.SetWorkExperience("2013-2015", "哇哈哈");
+
+            a.Display();
+            b.Display();
+            Console.Read();
+        }
+    }
+}
---
+++ Prototypes/Resume.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototypes
+{
+    /// <summary>
+    /// 简历类，实现ICloneable接口
+    /// </summary>
+    class Resume : ICloneable
+    {
+        private string _name;
+        private string _sex;
+        private int _age;
+        WorkExperience WorkArea { get; set; }
+        public string Name { get => _name; set => _name = value; }
+        public string Sex { get => _sex; set => _sex = value; }
+        public int Age { get => _age; set => _age = value; }
+
+        public Resume(string name)
+        {
+            Name = name;
+            WorkArea = new WorkExperience();
+        }
+        /// <summary>
+        /// 私有构造方法，复制工作区域
+        /// </summary>
+        /// <param name="workArea"></param>
+        private Resume(WorkExperience workArea)
+        {
+            WorkArea = (WorkExperience)workArea.Clone();
+        }
+        /// <summary>
+        /// 设置个人信息
+        /// </summary>
+        /// <param name="sex"></param>
+        /// <param name="age"></param>
+        public void SetPersonalInfo(string sex, int age)
+        {
+            Sex = sex;
+            Age = age;
+        }
+
+        /// <summary>
+        /// 设置工作经历
+        /// </summary>
+        /// <param name="timeArea"></param>
+        /// <param name="company"></param>
+        public void SetWorkExperience(string timeArea, string company)
+        {
+            WorkArea.TimeArea = timeArea;
+            WorkArea.Company = company;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("{0}{1}{2}", Name, Sex, Age);
+            Console.WriteLine("工作经历：{0} {1}", WorkArea.TimeArea, WorkArea.Company);
+
+        }
+
+        public object Clone()
+        {
+            //添加深复制,调用私有构造方法，克隆“工作区域”,然后给简历对象赋值，最后返回一个深复制的简历对象
+            Resume obj = new Resume(WorkArea);
+            obj.Name = this._name;
+            obj.Sex = _sex;
+            obj.Age = _age;
+
+
+            //return (object)this.MemberwiseClone();
+            return obj;
+        }
+    }
+    /// <summary>
+    /// 工作区域类
+    /// </summary>
+    class WorkExperience : ICloneable
+    {
+        private string _timeArea;
+        private string _company;
+
+        public string TimeArea { get => _timeArea; set => _timeArea = value; }
+        public string Company { get => _company; set => _company = value; }
+
+        public object Clone()
+        {
+            return (WorkExperience)this.MemberwiseClone();
+        }
+    }
+}

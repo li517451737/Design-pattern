28b2db17c71bad864889aaf3268fc4c438c72505
孤鸿 夜 <litiansum@foxmail.com>
2017-10-23 21:56:55 +0800

    添加外观模式

---
+++ Facades/Program.cs
@@ -0,0 +1,28 @@
+﻿/***
+ * 外观模式：为子系统的一组接口提供一个一致的界面，此模式定义了一个高层接口，这个接口使得子系统更加容易使用。
+ * 
+ * 外观模式应用场景：
+ * 1. 在设计初期，应该有意识的将不同的两个层分离，在层与层之间建立外观Facade
+ * 2. 在开发阶段，子系统往往因为不断的重购演化而变得越来越复杂，增加外观Facade可以提供一个简单的接口，减少它们的依赖。
+ * 3. 在维护遗留的大型系统时，可以为新系统开发一个Facade类，来提供设计粗糙或高度复杂的遗留代码的比较清晰的接口，让新系统与Facade交互，Facade与遗留代码交互所有复杂的工作。
+ * 
+ */ 
+
+
+
+using System;
+
+namespace Facades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //外观类
+            Facade facade = new Facade();
+            facade.MethodA();
+            facade.MethodB();
+            Console.Read();
+        }
+    }
+}
---
+++ Facades/SubSystem.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facades
+{
+    /**
+     * 子系统类集合，实现子系统功能，处理Facade对象指派任务，子类中没有任何Facade的引用
+     * 
+     */
+
+    /// <summary>
+    /// 子系统一
+    /// </summary>
+    class SubSystemOne
+    {
+        public void MethodOne()
+        {
+            Console.WriteLine("子系统方法一");
+        }
+    }
+    /// <summary>
+    /// 子系统二
+    /// </summary>
+    class SubSystemTwo
+    {
+        public void MethodTwo()
+        {
+            Console.WriteLine("子系统方法二");
+        }
+    }
+
+    /// <summary>
+    /// 子系统三
+    /// </summary>
+    class SubSystemThree
+    {
+        public void MethodThree()
+        {
+            Console.WriteLine("子系统方法三");
+        }
+    }
+    /// <summary>
+    /// 子系统四
+    /// </summary>
+    class SubSystemFour
+    {
+        public void MethodFour()
+        {
+            Console.WriteLine("子系统方法四");
+        }
+    }
+    /// <summary>
+    /// 外观类
+    /// </summary>
+    class Facade
+    {
+        SubSystemOne one;
+
+        SubSystemTwo two;
+
+        SubSystemThree three;
+
+        SubSystemFour four;
+
+        /// <summary>
+        /// 外观类，它需要了解所有子系统的方法或属性进行组合，以便外界调用
+        /// </summary>
+        public Facade()
+        {
+            one = new SubSystemOne();
+            two = new SubSystemTwo();
+            three = new SubSystemThree();
+            four = new SubSystemFour();
+        }
+
+        public void MethodA()
+        {
+            Console.WriteLine("方法组A:");
+            one.MethodOne();
+            two.MethodTwo();
+            four.MethodFour();
+        }
+
+        public void MethodB()
+        {
+            Console.WriteLine("方法组B:");
+            one.MethodOne();
+            three.MethodThree();
+        }
+    }
+}

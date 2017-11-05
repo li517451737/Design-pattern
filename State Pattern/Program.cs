439728d9570b8a4960494f827489e55cb31a916f
孤鸿 夜 <litiansum@foxmail.com>
2017-11-05 20:50:04 +0800

    添加状态模式

---
+++ State Pattern/Program.cs
@@ -0,0 +1,28 @@
+﻿/**
+ * 状态模式：当一个对象的内在状态改变时允许改变其行为，这个对象看起来像改变其类。
+ * 主要解决的是当控制一个对象状态的条件表达式过于复杂时的情况。把状态的判断逻辑转移到表示不同状态的一系列类中，可以把复杂的判断逻辑简化。
+ * 
+ * 当一个对象的行为取决于它的状态，并且它必须在运行时刻根据状态改变行为时，就可以考虑使用状态模式。
+ * 
+ * 
+ */
+
+using StatePattern;
+using System;
+
+namespace State_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context = new Context(new ConcreteStateA());
+            context.Request();
+            context.Request();
+            context.Request();
+            context.Request();
+
+            Console.Read();
+        }
+    }
+}
---
+++ State Pattern/State.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatePattern
+{
+    /// <summary>
+    /// 状态类，定义一个接口或抽象类，与Context的一个特定状态相关的行为。
+    /// </summary>
+    abstract class State
+    {
+        public abstract void Handle(Context context);
+    }
+
+    /// <summary>
+    /// 具体状态类，每个子类实现一个与Context状态相关的行为
+    /// </summary>
+    class ConcreteStateA : State
+    {
+        public override void Handle(Context context)
+        {
+            //设置ConcreteStateA的下一个状态是ConcreteStateB
+            context.State = new ConcreteStateB();
+        }
+    }
+
+    class ConcreteStateB : State
+    {
+        public override void Handle(Context context)
+        {
+            //设置ConcreteStateB的下一个状态是ConcreteStateA
+            context.State = new ConcreteStateA();
+        }
+    }
+
+    /// <summary>
+    /// Context类，维护State子类的实例，定义当前状态的对象
+    /// </summary>
+    class Context
+    {
+        private State _state;
+
+        internal State State
+        {
+            get => _state;
+            set
+            {
+                _state = value;
+                Console.WriteLine("当前状态为：" + _state.GetType().Name);
+            }
+        }
+
+        public Context(State state)
+        {
+            State = state;
+        }
+
+        public void Request()
+        {
+            _state.Handle(this);
+        }
+
+    }
+}

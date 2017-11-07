75c193af11da284e88ce7677e333798eb4f1ad05
孤鸿 夜 <litiansum@foxmail.com>
2017-11-07 20:38:52 +0800

    添加备忘录（Memento）模式

---
+++ Memento/Originator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Memento
+{
+    /// <summary>
+    /// 发起人
+    /// </summary>
+    class Originator
+    {
+        private string state;
+
+        public string State { get => state; set => state = value; }
+
+        /// <summary>
+        /// 创建备忘录，将当前需要保存的数据信息导入并实例化一个Memento对象
+        /// </summary>
+        /// <returns></returns>
+        public Memento CreateMemento()
+        {
+            return new Memento(state);
+        }
+        /// <summary>
+        /// 恢复备忘录，将Memento导入并恢复相关数据
+        /// </summary>
+        /// <param name="memento"></param>
+        public void SetMemento(Memento memento)
+        {
+            state = memento.State;
+        }
+        /// <summary>
+        /// 显示数据
+        /// </summary>
+        public void Show()
+        {
+            Console.WriteLine("当前状态为：" + state);
+        }
+
+    }
+
+    class Memento
+    {
+        private string state;
+        public Memento(string state)
+        {
+            this.state = state;
+        }
+        /// <summary>
+        /// 需要保存的数据属性，可以是多个对象
+        /// </summary>
+        public string State
+        {
+            get { return state; }
+        }
+    }
+    /// <summary>
+    /// 管理者
+    /// </summary>
+    class Caretaker
+    {
+        /// <summary>
+        /// 备忘录属性，得到或设置备忘录
+        /// </summary>
+        public Memento Memento
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Memento/Program.cs
@@ -0,0 +1,31 @@
+﻿/**
+ * 备忘录模式：在不破坏封装性的前提下，捕获一个对象的内部状态，并在该对象之外保存这个状态。这样以后就可以将该对象恢复到原先保存的状态。
+ * 
+ * 备忘录模式比较适用于功能比较复杂的，但需要维护或记录属性历史的类，或需要保存的属性只是众多属性中的一小部分时，Originator可以根据保存的Memento信息还原到前一状态。
+ * 例如：当角色状态改变时，有可能这个状态无效，这时候就可以使用暂时储存起来的备忘录将角色状态还原。
+ */ 
+using System;
+
+namespace Memento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Originator o = new Originator();
+            o.State = "On"; //Originator初始状态为 “On”
+            o.Show();
+
+            Caretaker c = new Caretaker();
+            c.Memento = o.CreateMemento();
+
+            o.State = "Off"; //改变状态属性
+            o.Show();
+
+            o.SetMemento(c.Memento); // 通过备忘录恢复初始状态
+            o.Show();
+
+            Console.Read();
+        }
+    }
+}

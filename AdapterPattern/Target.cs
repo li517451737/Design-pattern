815d99341bbed3d0fe9dd3930d46c8766738b133
孤鸿 夜 <litiansum@foxmail.com>
2017-11-07 19:58:19 +0800

    添加适配器模式

---
+++ AdapterPattern/Target.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdapterPattern
+{
+    /// <summary>
+    /// 客户所期待的接口。目标可以是具体的或抽象的类，也可以是接口
+    /// </summary>
+    class Target
+    {
+        public virtual void Request()
+        {
+            Console.WriteLine("普通请求！");
+        }
+    }
+
+    /// <summary>
+    /// 需要适配的类
+    /// </summary>
+    class Adaptee
+    {
+        public void SpecificRequeset()
+        {
+            Console.WriteLine("特殊请求！");
+        }
+
+    }
+    /// <summary>
+    /// 适配器类，在内部包装一个Adaptee对象，将源接口转换为目标接口
+    /// </summary>
+    class Adapter : Target
+    {
+        private Adaptee adaptee = new Adaptee();
+
+        public override void Request()
+        {
+            adaptee.SpecificRequeset();
+        }
+    }
+}

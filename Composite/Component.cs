86615e36f2488a43cf7f866ac93e039d10abb3e6
孤鸿 夜 <litiansum@foxmail.com>
2017-11-08 20:57:33 +0800

    添加组合模式

---
+++ Composite/Component.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composite
+{
+    /// <summary>
+    /// 为组合中的对象声明接口，在适当情况下，实现所有类共有接口的默认行为。
+    /// 声明一个接口用于访问和管理的Component的子部件
+    /// </summary>
+    abstract class Component
+    {
+        protected string name;
+
+        public Component(string name)
+        {
+            this.name = name;
+        }
+
+        public abstract void Add(Component component);
+        public abstract void Remove(Component component);
+        public abstract void Display(int depth);
+
+    }
+
+    /// <summary>
+    /// 在组合表示叶节点对象，叶节点没有子节点
+    /// </summary>
+    class Leaf : Component
+    {
+
+        public Leaf(string name) : base(name) { }
+
+        //由于叶子节点没有再增加子节点，所有Add和Remove方法实现它没有意义，但着这样可以消除叶节点和枝节点对象在抽象层次的却别，他们具有完全一致的接口。
+
+        public override void Add(Component component)
+        {
+            Console.WriteLine("不能给叶节点添加子节点");
+        }
+
+        public override void Display(int depth)
+        {
+            Console.WriteLine(new String('-', depth) + name);
+        }
+
+        public override void Remove(Component component)
+        {
+            Console.WriteLine("不能从叶节点删除子节点");
+
+        }
+    }
+    /// <summary>
+    /// 定义有枝节点的行为
+    /// </summary>
+    class Composite : Component
+    {
+        /// <summary>
+        /// 子对象集合，用来储存下属枝节点和叶节点
+        /// </summary>
+        private List<Component> children = new List<Component>();
+
+        public Composite(string name) : base(name) { }
+
+        public override void Add(Component component)
+        {
+            children.Add(component);
+        }
+
+        public override void Display(int depth)
+        {
+            Console.WriteLine(new String('-', depth) + name);
+            foreach (var item in children)
+            {
+                item.Display(depth + 2);
+            }
+        }
+
+        public override void Remove(Component component)
+        {
+            children.Remove(component);
+        }
+    }
+
+}
---
+++ Composite/Program.cs
@@ -0,0 +1,39 @@
+﻿/**
+ * 组合模式：将对象组合成树形结构，以表示“部分--整体”的结构层次。组合模式使得用户对单个对象和组合对象的使用具有一致性。
+ * 
+ * 透明方式：在Component中声明所有用来管理子对象的方法。这样实现Component接口的枝节点和叶节点对于外界没有区别，它们具有完全一致的行为接口。
+ * 当需求是提现部分与整体层次的结构时，并且希望用户可以忽略组合对象和单个对象的不同，统一地使用组合结构中的所有对象时，就可以考虑组合模式。 
+ * 例如：公司的组织结构等等。
+ * 
+ */ 
+
+using System;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Composite root = new Composite("root");//初始化根节点并添加两个叶节点
+            root.Add(new Leaf("Leaf A"));  
+            root.Add(new Leaf("Leaf B"));
+
+            //声明两个枝节点，并在枝节点上，添加两个叶节点
+
+            Composite comp1 = new Composite("Composite X");
+            comp1.Add(new Leaf("LeafXA"));
+            comp1.Add(new Leaf("LeafXB"));
+            root.Add(comp1);
+
+            Composite comp2 = new Composite("Composite Y");
+            comp2.Add(new Leaf("LeafYA"));
+            comp2.Add(new Leaf("LeafYB"));
+            root.Add(comp2);
+            
+            root.Display(1); //遍历所有节点
+
+            Console.Read();
+        }
+    }
+}

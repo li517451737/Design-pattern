2dbe13aeb98eab2c60b8c5a104fcf3e4f7caeba0
孤鸿 夜 <litiansum@foxmail.com>
2017-11-09 20:36:44 +0800

    添加迭代器模式

---
+++ Iterator/Iterator.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iterator
+{
+    /// <summary>
+    /// Iterator迭代器抽象类
+    /// </summary>
+    abstract class Iterator
+    {
+        /// <summary>
+        /// 第一个对象
+        /// </summary>
+        /// <returns></returns>
+        public abstract object First();
+        /// <summary>
+        /// 下一个对象
+        /// </summary>
+        /// <returns></returns>
+        public abstract object Next();
+        /// <summary>
+        /// 是否到结尾
+        /// </summary>
+        /// <returns></returns>
+        public abstract bool IsDone();
+        /// <summary>
+        /// 当前对象
+        /// </summary>
+        /// <returns></returns>
+        public abstract object CurrentItem();
+
+    }
+    /// <summary>
+    /// 聚集抽象类
+    /// </summary>
+    abstract class Aggregate
+    {
+        /// <summary>
+        /// 创建迭代器
+        /// </summary>
+        /// <returns></returns>
+        public abstract Iterator CreateIterator();
+    }
+    /// <summary>
+    /// 具体聚集类，继承Aggregate
+    /// </summary>
+    class ConcreteAggregate : Aggregate
+    {
+        private IList<object> items = new List<object>();
+
+        public override Iterator CreateIterator()
+        {
+            return new ConcreteIterator(this);
+        }
+        /// <summary>
+        /// 返回聚集总数
+        /// </summary>
+        public int Count
+        {
+            get { return items.Count; }
+        }
+        /// <summary>
+        /// 声明一个索引
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public object this[int index]
+        {
+            get { return items[index]; }
+            set { items.Insert(index, value); }
+        }
+    }
+
+    /// <summary>
+    /// 具体迭代类
+    /// </summary>
+    class ConcreteIterator : Iterator
+    {
+        private ConcreteAggregate aggregate;
+        private int current = 0;
+        public ConcreteIterator(ConcreteAggregate aggregate)
+        {
+            this.aggregate = aggregate;
+        }
+
+        public override object CurrentItem()
+        {
+            return aggregate[current];
+        }
+
+        public override object First()
+        {
+            return aggregate[0];
+        }
+
+        public override bool IsDone()
+        {
+            return current >= aggregate.Count ? true : false;
+        }
+
+        public override object Next()
+        {
+            object ret = null;
+            current++;
+            if (current < aggregate.Count)
+            {
+                ret = aggregate[current];
+            }
+            return ret;
+        }
+    }
+
+}
---
+++ Iterator/Program.cs
@@ -0,0 +1,34 @@
+﻿/**
+ * 迭代器模式：提供一种顺序访问一个聚合对象中的各个元素，而又不暴露该对象的内部表示。
+ * 迭代器模式就是分离了集合对象的遍历行为，抽象出一个迭代器类来负责，这样既可以做到不暴露集合内部结构，又可以让外部代码透明地访问集合内部的数据。
+ * .net中的IEumerator支持对非泛型集合的简单迭代
+ * IEnumerator：是一个真正的集合访问器，提供在普通集合中遍历的接口，有Current，MoveNext()，Reset()，其中Current返回的是object类型。
+ * IEnumerable： 暴露一个IEnumerator（只包含一个抽象的方法GetEnumerator(),该方法返回一个可用于循环访问集合的IEnumerator对象），支持在普通集合中的遍历。
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcreteAggregate a = new ConcreteAggregate();
+            a[0] = "张三";
+            a[1] = "李四";
+            a[2] = "王二";
+            a[3] = "哇哈哈";
+            a[4] = "乐事";
+            Iterator iterator = new ConcreteIterator(a);
+            while (!iterator.IsDone())
+            {
+                Console.WriteLine("{0} 请买票", iterator.CurrentItem());
+                iterator.Next();
+            }
+
+            Console.Read();
+        }
+    }
+}

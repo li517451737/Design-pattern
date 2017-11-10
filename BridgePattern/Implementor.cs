d1b1828ae14ee88f660cf56728bfdf1ea1796e28
孤鸿 夜 <litiansum@foxmail.com>
2017-11-10 21:32:39 +0800

    添加桥接模式

---
+++ BridgePattern/Implementor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgePattern
+{
+    /// <summary>
+    /// 抽象实现类
+    /// </summary>
+    abstract class Implementor
+    {
+        public abstract void Operation();
+    }
+
+    /// <summary>
+    /// 具体实现类
+    /// </summary>
+    class ConcreteImplementorA : Implementor
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("具体实现类A");
+        }
+    }
+
+    class ConcreteImplementorB : Implementor
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("具体实现类B");
+        }
+    }
+
+    class Abstraction
+    {
+        protected Implementor implementor;
+
+        public void SetImplementor(Implementor implementor)
+        {
+            this.implementor = implementor;
+        }
+
+        public virtual void Operation()
+        {
+            implementor.Operation();
+        }
+    }
+    /// <summary>
+    /// 被提炼的抽象对象
+    /// </summary>
+    class RefinedAbstraction : Abstraction
+    {
+        public override void Operation()
+        {
+            base.Operation();
+        }
+    }
+}
---
+++ BridgePattern/Program.cs
@@ -0,0 +1,25 @@
+﻿/**
+ * 桥接模式：将抽象部分和它的实现部分分离，使它们都可以独立变化。
+ * 说明：抽象和它的实现分离，并不是说，让其抽象类与其派生类分离。实现指的是抽象类和它的派生类用来实现自己的对象。
+ * 
+ * 
+ */ 
+
+using System;
+
+namespace BridgePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Abstraction ab = new RefinedAbstraction();
+            ab.SetImplementor(new ConcreteImplementorA());
+            ab.Operation();
+
+            ab.SetImplementor(new ConcreteImplementorB());
+            ab.Operation();
+            Console.Read();
+        }
+    }
+}

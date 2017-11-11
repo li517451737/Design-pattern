7b2a798539f740bf6373f5e23cb547a2092daca4
孤鸿 夜 <litiansum@foxmail.com>
2017-11-11 20:42:38 +0800

    添加命令模式

---
+++ CommandPattern/Command.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandPattern
+{
+    /// <summary>
+    /// 声明命令执行接口
+    /// </summary>
+    abstract class Command
+    {
+        protected Receiver receiver;
+
+        public Command(Receiver receiver)
+        {
+            this.receiver = receiver;
+        }
+
+        public abstract void Excute();
+    }
+    /// <summary>
+    /// 声明一个实施和执行一个请求相关的操作，任何类都可能作为一个接收者
+    /// </summary>
+    class Receiver
+    {
+        public void Action()
+        {
+            Console.WriteLine("");
+        }
+    }
+
+    /// <summary>
+    /// 将一个接收者对象绑定于一个动作，调用接受者相应的操作，以实现Execute。
+    /// </summary>
+    class ConcreteCommand : Command
+    {
+        public ConcreteCommand(Receiver receiver) : base(receiver)
+        {
+            this.receiver = receiver;
+        }
+
+
+        public override void Excute()
+        {
+            receiver.Action();
+        }
+    }
+    /// <summary>
+    /// 要求该命令执行这个请求
+    /// </summary>
+    class Invoke
+    {
+        private Command command;
+
+        public void SetCommand(Command command)
+        {
+            this.command = command;
+        }
+
+        public void ExcuteCommand()
+        {
+            command.Excute();
+        }
+    }
+}
---
+++ CommandPattern/Program.cs
@@ -0,0 +1,31 @@
+﻿/**
+ * 命令模式：将一个请求封装为一个对象，从而使你可以用不同的请求对客户进行参数化；对请求排队或记录请求日志，以及支持可撤销的操作。
+ * 命令模式的作用：
+ * 1. 它能较容易的设计一个命令队列；
+ * 2. 在需要的情况下，可以较容易的将命令记录到日志；
+ * 3. 允许接收请求的一方决定是否要否决请求，很容易地实现对请求的撤销和重做；
+ * 4. 由于加进新的具体命令类不影响其他的类，因此增加新的具体命令类很容易；
+ * 5. 命令模式把请求一个操作的对象与知道怎么执行一个操作的对象分割开。
+ * 
+ * 
+ */ 
+using System;
+
+namespace CommandPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //基本结构
+            
+            Receiver r = new Receiver();
+            Command command = new ConcreteCommand(r);
+            Invoke i = new Invoke();
+            i.SetCommand(command);
+            i.ExcuteCommand();
+
+            Console.Read();
+        }
+    }
+}

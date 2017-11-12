55acb425cea75cbd8d0ec2578bbbbc87fb263d84
孤鸿 夜 <litiansum@foxmail.com>
2017-11-12 20:53:03 +0800

    添加中介者模式

---
+++ Mediators/Mediator.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mediators
+{
+    /// <summary>
+    /// 抽象中介者类 定义同事对象到中介者对象的接口
+    /// </summary>
+    public abstract class Mediator
+    {
+        public abstract void SendMessage(string message, Colleague colleague);
+    }
+
+    /// <summary>
+    /// 抽象同事类
+    /// </summary>
+    public abstract class Colleague
+    {
+        protected Mediator mediator;
+
+        /// <summary>
+        /// 构造方法，通过构造方法得到中介者对象
+        /// </summary>
+        /// <param name="mediator"></param>
+        public Colleague(Mediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+    }
+    /// <summary>
+    /// 具体中介者类   包含所有具体同事对象
+    /// </summary>
+    public class ConcreteMediator : Mediator
+    {
+        private ConcreteColleagueA colleagueA;
+        private ConcreteColleagueB colleagueB;
+
+        public ConcreteColleagueA ColleagueA { set => colleagueA = value; }
+        public ConcreteColleagueB ColleagueB { set => colleagueB = value; }
+
+        /// <summary>
+        /// 重写发送消息方法，根据发送者对象，做出选择判断，通知对象
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="colleague"></param>
+        public override void SendMessage(string message, Colleague colleague)
+        {
+            if (colleague == colleagueA)
+            {
+                colleagueB.Notify(message);
+            }
+            else
+            {
+                colleagueA.Notify(message);
+            }
+        }
+    }
+    /// <summary>
+    /// 具体同事A
+    /// </summary>
+    public class ConcreteColleagueA : Colleague
+    {
+        public ConcreteColleagueA(Mediator mediator) : base(mediator)
+        {
+        }
+
+        public void SendMessage(string message)
+        {
+            mediator.SendMessage(message, this);
+        }
+
+        public void Notify(string messgae)
+        {
+            Console.WriteLine("同事A得到消息：" + messgae);
+        }
+    }
+    /// <summary>
+    /// 同事B
+    /// </summary>
+    public class ConcreteColleagueB : Colleague
+    {
+        public ConcreteColleagueB(Mediator mediator) : base(mediator)
+        {
+
+        }
+
+        public void SendMessage(string message)
+        {
+            mediator.SendMessage(message, this);
+        }
+
+        public void Notify(string messgae)
+        {
+            Console.WriteLine("同事B得到消息：" + messgae);
+        }
+    }
+}
---
+++ Mediators/Program.cs
@@ -0,0 +1,35 @@
+﻿/**
+ * 中介者模式：用一个中介对象来封装一系列对象的交互。中介者使各个对象不需要显示地相互引用，从而使其耦合松散，而且可以独立的改变它们间的交互。     
+ * 
+ * 中介者模式一般应用于一组对象以定义良好但复杂的方式进行通信的场合，以及想定制一个分布在多个类中的行为，而又不想生成太多子类的情况。
+ * 中介者模式很容易在系统中应用，也和容易在系统中误用。当系统出现“多对多”交互复杂的对象群时，不要急于使用中介者模式，而要先反思你的系统在设计上是否合理。
+ * 
+ * 优点：
+ *  Mediator的出现减少了各个Colleague的耦合，使得可以独立的改变和复用哥哥Colleague类和Mediator对象。
+ *  
+ *  缺点：
+ *  由于ConcreteMediator控制了集中化，于是就把交互复杂性变为了中介者的复杂性，就使得中介者会变得比任何一个ConcreteCollegue都要复杂。
+ */ 
+using System;
+
+namespace Mediators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcreteMediator m = new ConcreteMediator();
+            ConcreteColleagueA c1 = new ConcreteColleagueA(m);
+            ConcreteColleagueB c2 = new ConcreteColleagueB(m);
+
+            m.ColleagueA = c1;
+            m.ColleagueB = c2;
+
+            c1.SendMessage("吃饭了吗？");
+            c2.SendMessage("刚刚吃了，你呢？");
+
+
+            Console.Read();
+        }
+    }
+}

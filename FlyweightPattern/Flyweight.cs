2f72061519a152ba1e07300da0e1b4b29c79a40a
孤鸿 夜 <litiansum@foxmail.com>
2017-11-13 22:29:14 +0800

    添加享元模式

---
+++ FlyweightPattern/Flyweight.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlyweightPattern
+{
+    /// <summary>
+    /// 所有具体享元类的超类或接口，通过这个接口，Flyweight可以接受并作用于外部状态
+    /// </summary>
+    abstract class Flyweight
+    {
+        public abstract void Operation(int extrinsicstate);
+    }
+    /// <summary>
+    /// 继承Flyweight，并为内部状态增加储存空间。
+    /// </summary>
+    class ConcreteFlyweight : Flyweight
+    {
+        public override void Operation(int extrinsicstate)
+        {
+            Console.WriteLine("具体共享对象：" + extrinsicstate);
+        }
+    }
+    /// <summary>
+    /// 实现不需要共享的子类。因为Flyweight接口使共享成为可能，但它并不强制共享
+    /// </summary>
+    class UnshardConcreteFlyweight : Flyweight
+    {
+        public override void Operation(int extrinsicstate)
+        {
+            Console.WriteLine("不共享的具体对象：" + extrinsicstate);
+
+        }
+    }
+    /// <summary>
+    /// 享元工厂，用来创建和管理Flyweight对象
+    /// </summary>
+    class FlyweightFactory
+    {
+        private Hashtable flyweights = new Hashtable();
+
+        /// <summary>
+        /// 根据客户端请求，获取享元实例
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public Flyweight GetFlyweight(string key)
+        {
+            if (!flyweights.ContainsKey(key))
+            {
+                flyweights.Add(key, new ConcreteFlyweight());
+            }
+            return (Flyweight)flyweights[key];
+        }
+    }
+}
---
+++ FlyweightPattern/Program.cs
@@ -0,0 +1,37 @@
+﻿/**
+ * 享元模式（Flyweight）：运用共享技术有效地支持大量细粒度的对象。
+ * 
+ * 享元模式可以避免大量非常相似类的开销。在程序设计中，有时候需要生成大量细粒度的类的实例来表示数据。如果能发现这些实例除了几个参数外基本都是相同的，
+ * 有时候就能够大幅度减少需要实例化类的数量。如果把那些参数移到类的实例外面去，在方法调用时将它们传递进来，就可以通过共享，大幅度减少单个实例的数量。
+ * 
+ * 如果一个应用程序使用了大量的对象，而大量的对象造成了很大的储存开销时，就可以考虑使用享元模式。
+ */ 
+
+using System;
+
+namespace FlyweightPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int extrinsicstate = 12;
+            FlyweightFactory factory = new FlyweightFactory();
+
+            Flyweight fx = factory.GetFlyweight("X");
+            fx.Operation(--extrinsicstate);
+
+            Flyweight fy = factory.GetFlyweight("Y");
+            fy.Operation(--extrinsicstate);
+
+            Flyweight fz = factory.GetFlyweight("Z");
+            fz.Operation(--extrinsicstate);
+
+            UnshardConcreteFlyweight uf = new UnshardConcreteFlyweight();
+            uf.Operation(--extrinsicstate);
+
+
+            Console.Read();
+        }
+    }
+}
